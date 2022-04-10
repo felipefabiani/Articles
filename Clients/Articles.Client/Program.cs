d10095b3e3395cd7ad3ce0c95931bbcf987e7912
FelipeFabiani <felipefabiani@gmail.com>
2022-04-10 11:23:20 +0100

    Refactoring adding fluent validators

--- Clients/Articles.Client/Program.cs
+++ Clients/Articles.Client/Program.cs
@@ -1,19 +1,15 @@
 using Articles.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Serilog;
 using Serilog.Debugging;
-using MudBlazor.Services;
-using FluentValidation;
-using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
 
 SelfLog.Enable(m => Console.Error.WriteLine(m));
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.BrowserConsole()
     .CreateLogger();
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -30,42 +26,7 @@ builder.Services.AddHttpClient<ArticleApiClient>(client =>
 
 });
 
-List<Assembly> listOfAssemblies = new List<Assembly>();
-var mainAsm = Assembly.GetEntryAssembly();
-listOfAssemblies.Add(mainAsm);
-
-foreach (var refAsmName in mainAsm.GetReferencedAssemblies())
-{
-    listOfAssemblies.Add(Assembly.Load(refAsmName));
-}
-
-
-var ass = listOfAssemblies
-    .Where(x => x.FullName.Contains("Articles"));
-
-var validators = listOfAssemblies
-    .SelectMany(x => x.GetTypes())
-    .ToList();
-
-var validators1 = validators
-    // .Where(x => x.Name == "UserLoginRequestValidator")
-    // .Where(t => t.IsAssignableTo(typeof(AbstractValidator<UserLoginRequest>)))
-    .Where(x => x.FullName.Contains("Articles"))
-    .Where(t =>
-        t.BaseType is not null &&
-        !t.IsAbstract &&
-        t.BaseType.IsGenericType &&
-        t.BaseType.GetGenericTypeDefinition() == typeof(AbstractValidator<>))
-    .ToList();
-
-foreach (var item in validators1)
-{
-    var arg = item.BaseType!.GetGenericArguments().First();
-    var genericInterface = typeof(AbstractValidator<>).MakeGenericType(arg);
-    builder.Services.AddSingleton(genericInterface, item);
-}
-
-
+builder.AddFluentValidators("Articles.Models");
 
 Log.Debug("Setting Oidc");
 builder.Services.AddOidcAuthentication(options =>
---
+++ Clients/Articles.Client/ProgramExtension.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Reflection;
+
+namespace Articles.Client;
+public static class WebAssemblyHostExtension
+{
+    public static void AddFluentValidators(this WebAssemblyHostBuilder builder, string assemblyName)
+    {
+        AddFluentValidators(builder, new List<string> { assemblyName });
+    }
+    public static void AddFluentValidators(this WebAssemblyHostBuilder builder, List<string> assemblyNames)
+    {
+        var validators = Assembly
+            .GetEntryAssembly()!
+            .GetReferencedAssemblies()
+            .Where(assembly => assemblyNames.Any(name => assembly.FullName.Contains(name)))
+            .Select(assembly => Assembly.Load(assembly))
+            .SelectMany(assembly => assembly
+                .GetTypes()
+                .Where(t =>
+                    !t.IsAbstract &&
+                    t.BaseType is not null &&
+                    t.BaseType.IsGenericType &&
+                    t.BaseType.GetGenericTypeDefinition() == typeof(AbstractValidator<>)))
+            .ToList();
+
+        foreach (var validator in validators)
+        {
+            var arg = validator.BaseType!.GetGenericArguments().First();
+            var baseType = typeof(AbstractValidator<>).MakeGenericType(arg);
+            builder.Services.AddSingleton(baseType, validator);
+        }
+    }
+}
