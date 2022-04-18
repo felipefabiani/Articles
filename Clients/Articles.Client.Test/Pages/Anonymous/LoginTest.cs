845f54c6d30f3504d282f4af2be1d17756770b8e
FelipeFabiani <felipefabiani@gmail.com>
2022-04-18 11:13:04 +0100

    Add Test for blazor components.

--- Clients/Articles.Client.Test/Pages/Anonymous/LoginTest.cs
+++ Clients/Articles.Client.Test/Pages/Anonymous/LoginTest.cs
@@ -1,14 +1,33 @@
 ﻿using Articles.Client.Pages.Anonymous;
 using Articles.Client.Test.EndToEnd;
+<<<<<<< HEAD
 using Articles.Models.Feature.Login;
 using AutoFixture;
 using Bunit;
+=======
+using Articles.Models.Auth;
+using Articles.Models.Feature.Login;
+using AutoFixture;
+using Bunit;
+using Bunit.TestDoubles;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+>>>>>>> Add Test for blazor components.
 using MudBlazor;
 using Shouldly;
 using System;
 using System.Collections.Generic;
+<<<<<<< HEAD
 using System.Net;
 using Xunit;
+=======
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Xunit;
+using ssc = System.Security.Claims;
+>>>>>>> Add Test for blazor components.
 
 namespace Articles.Client.Test.Pages.Anonymous;
 
@@ -70,6 +89,7 @@ public class LoginTest : IDisposable
         // Arrange
         _ctxFixture.AddHttpClient(
            response: new BadRequestResponse
+<<<<<<< HEAD
         {
             statusCode = 400,
             message = "One or more errors occured!",
@@ -83,6 +103,21 @@ public class LoginTest : IDisposable
 
         }, 
           statusCode: HttpStatusCode.BadRequest, 
+=======
+           {
+               statusCode = 400,
+               message = "One or more errors occured!",
+               errors = new Errors
+               {
+                   GeneralErrors = new string[]
+                {
+                    "User or password incorrect!"
+                }
+               }
+
+           },
+          statusCode: HttpStatusCode.BadRequest,
+>>>>>>> Add Test for blazor components.
           endpoint: "api/login");
 
         var cut = _ctxFixture.Context.RenderComponent<Login>();
@@ -107,6 +142,7 @@ public class LoginTest : IDisposable
         string password)
     {
         // Arrange
+<<<<<<< HEAD
         var ret = _fixture.Create<UserLoginResponse>();
         _ctxFixture.AddHttpClient(
             response: ret,
@@ -114,6 +150,36 @@ public class LoginTest : IDisposable
             endpoint: "api/login");
 
         var cut = _ctxFixture.Context.RenderComponent<Login>();
+=======
+        var resp = new UserLoginResponse
+        {
+            FullName = "Admin Test",
+            UserRoles = new List<string> { "admin" },
+            UserClaims = new List<string> { "Test" },
+            Token = new JwtToken
+            {
+                ExpiryDate = DateTime.UtcNow.AddHours(4),
+                Value = CreateToken(
+                    signingKey: Guid.NewGuid().ToString(),
+                    expireAt: DateTime.Now.AddHours(4),
+                    roles: new List<string> { "Admin" },
+                    claims: new List<ssc.Claim> { new ssc.Claim("Article_Moderate", "100") },
+                    firstName: "Admin",
+                    lastName: "Test")
+                }
+        };
+        _ctxFixture.AddHttpClient(
+            response: resp,
+            statusCode: HttpStatusCode.OK,
+            endpoint: "api/login");
+
+        var returnUrl = "counter";
+        var cut = _ctxFixture.Context.RenderComponent<Login>(parameters =>
+            parameters.Add(p => p.ReturnUrl, returnUrl));
+
+        var navMan = _ctxFixture.Context.Services.GetRequiredService<FakeNavigationManager>();
+        navMan.NavigateTo("http://localhost/api/login");
+>>>>>>> Add Test for blazor components.
 
         // Act
         cut.Find("input#login-email").Change(email);
@@ -121,9 +187,50 @@ public class LoginTest : IDisposable
         await cut.Find("button#submit").ClickAsync(null!);
 
         // Assert
+<<<<<<< HEAD
         //var alert = cut.FindComponents<MudAlert>();
 
         //alert.Count.ShouldBe(1);
         //alert[0].Markup.ShouldContain("User or password incorrect!");
+=======
+        navMan.Uri.ShouldBe($"{navMan.BaseUri}{returnUrl}");
+    }
+
+    public string CreateToken(
+        string signingKey, 
+        DateTime? expireAt = null, 
+        IEnumerable<string>? permissions = null, 
+        IEnumerable<string>? roles = null, 
+        IEnumerable<ssc.Claim>? claims = null,
+        string? firstName = null,
+        string? lastName = null)
+    {
+        var list = new List<ssc.Claim>();
+        if (claims != null)
+        {
+            list.AddRange(claims);
+            list.Add(new ssc.Claim(ssc.ClaimTypes.Name, $"{firstName} {lastName}"));
+        }
+
+        if (permissions != null)
+        {
+            list.AddRange(permissions.Select((string p) => new ssc.Claim("permissions", p)));
+        }
+
+        if (roles != null)
+        {
+            list.AddRange(roles.Select((string r) => new ssc.Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", r)));
+        }
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ssc.ClaimsIdentity(list),
+            Expires = expireAt,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(signingKey)), "http://www.w3.org/2001/04/xmldsig-more#hmac-sha256")
+        };
+        var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        jwtSecurityTokenHandler.OutboundClaimTypeMap.Clear();
+        return jwtSecurityTokenHandler.WriteToken(jwtSecurityTokenHandler.CreateToken(tokenDescriptor));
+>>>>>>> Add Test for blazor components.
     }
 }
